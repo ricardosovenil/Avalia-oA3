fc47ec4ceaaa4d4ef885ab6d4174d833d4c7ef50
ricardosovenil <ricardo.aguiar@aluno.fmpsc.edu.br>
2024-11-26 09:58:57 -0300

    Delete Avaliação A3 directory

--- Avaliação A3/script.js
+++
@@ -1,7 +0,0 @@
-
-const links = document.querySelectorAll('a');
-links.forEach(link => {
-    link.addEventListener('click', () => {
-        alert('Você será redirecionado para ' + link.href);
-    });
-});
